b6ecac64319434e7d5c2e24b372e249b0e6b6754
Aleksandar Stojanovic <sale4319@gmail.com>
2021-06-15 17:39:41 +0200

    handeled basic navigation, added new icons

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -4,10 +4,19 @@ import { Routes } from '../constants/Routes';
 
 export const TitleBar = () => {
     const history = useHistory();
+
+    const handleHome = () => {
+        history.push(Routes.Home);
+        window.location.reload();
+    }
+    const handleBoards = () => {
+        history.push(Routes.Boards);
+        window.location.reload();
+    }
     return (
         <div className="container">
-            <img className="image" src="favicon.ico" alt="" onClick={() => history.push(Routes.Home)} />
-            <div className="title" onClick={() => history.push(Routes.Home)}>ReTrello</div>
+            <img className="image" src="boards.ico" alt="" onClick={handleBoards} />
+            <div className="title" onClick={handleHome} >ReTrello</div>
         </div>
     );
 };
